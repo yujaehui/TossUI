873f48946d77dfdce1796a811dc5f7cf14ee454f
Jaehui Yu <pkj0522@naver.com>
2024-04-15 22:47:54 +0900

    Initial Commit

---
+++ TossUI/TossUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossUIApp.swift
+//  TossUI
+//
+//  Created by Jaehui Yu on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TossUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
